d7d11103b5d3c166c367466deb91bf58f52db24d
Naveenkumar506 <naveennani0603@gmail.com>
2025-10-30 14:28:43 +0530

    Removed unwanted files and updated project structure

--- c1OSmodule/sampleEs6.js
+++
@@ -1,9 +0,0 @@
-//step-1 npm init -y Next step----> in packages.json add --->this "type":"module"
-
-import os from "os";
-
-console.log(os.freemem());
-console.log(os.arch());
-console.log(os.homedir());
-console.log(os.type());
-console.log(os.platform());
